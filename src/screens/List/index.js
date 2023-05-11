a699b4fe8535fa74639cd12e21a46f93fb006c7e
davif13 <davi.ffa@gmail.com>
2023-05-11 11:09:27 -0300

    feat: create initial logic to fetch the api with axios

--- src/screens/List/index.js
+++ src/screens/List/index.js
@@ -1,7 +1,37 @@
-
+import './index.css'
+import { getList } from "../../services/requests";
+import { useEffect, useState } from "react";
+import { ListCard } from '../../components/ListCard';
 
 export const ListScreen = () =>{
+  const [loading, setLoading] = useState(true);
+  const [listData, setListData] = useState([]);
+
+  const loadListItems = async () =>{
+    setLoading(true);
+    const result = await getList();
+    console.log(result);
+    setListData(result);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadListItems();
+  })
   return (
-    <div></div>
+    <div className="list-screen-container">
+      <div className="list-screen-content-container">
+        <div className="list-screen-header">
+          <img className='logo-image' src="/images/logo.png" alt="Supermarket List Logo"/>
+          <h1>Lista de Supermercado</h1>
+        </div>
+        <div className='list-screen-list-container'>
+          {loading && <h3>Carregando...</h3>}
+          {
+           !loading && listData?.length > 0 ? listData.map(item => <ListCard key={item._id} item={item} />) : <h3>Sua lista está vazia, crie um novo item clicando em "Adicionar"</h3>
+          }
+        </div>
+      </div>
+    </div>
   )
 }
